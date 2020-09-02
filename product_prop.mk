49599f8619d67602c15f7c82cc89b1f9ca704e97
SamarV-121 <samarvispute121@gmail.com>
2020-09-02 13:12:24 +0000

    universal7904: Switch to userspace LMKD
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,16 @@
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.latch_unsignaled=1
+
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true \
+    ro.lmk.log_stats=true

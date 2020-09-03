7065b5ae984cadd7d9dd42d204986b6148ba3811
erfanoabdi <erfangplus@gmail.com>
2020-09-03 16:03:20 +0000

    universal7904: Add bluetooth config
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,11 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
+
+#define BLE_VND_INCLUDED TRUE
+
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
+#endif

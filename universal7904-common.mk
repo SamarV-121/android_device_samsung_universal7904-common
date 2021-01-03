e40fa3f31cd4e79b56861229b22589de0b62c100
SamarV-121 <samarvispute121@gmail.com>
2021-01-03 14:32:35 +0530

    universal7904: Switch to pixel thermal HAL
    Change-Id: Ib728ab725f63145df4c72131c28c04d273d8edf4
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -213,8 +213,10 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-service.samsung
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Touch
 PRODUCT_PACKAGES += \

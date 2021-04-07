c8d1c156c9dd60eacb37f18b54c0eef8fd6ebdc5
SamarV-121 <samarvispute121@pm.me>
2021-04-07 16:20:39 +0200

    universal7904: Build 32bit camera provider
    Change-Id: Ia714795369be9cd74b804a830361135127a1aa72

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -42,7 +42,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service_64.universal7904 \
+    android.hardware.camera.provider@2.5-service.universal7904 \
     camera.exynos7904
 
 # ConfigStore

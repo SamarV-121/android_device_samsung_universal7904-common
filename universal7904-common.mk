1ca2d749cc7fe0893d099f791652a4724b6513b9
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    universal7904: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -239,6 +239,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.samsung

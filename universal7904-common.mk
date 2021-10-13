bfb82cf5a494db2626d388813eddde0105ea6b4f
SamarV-121 <samarvispute121@pm.me>
2021-10-13 20:27:48 +0530

    universal7904: Build needed vendor interface libraries
    Change-Id: I212841eca5cdcea5fb3199fb7e821f29c492e1f1
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -48,7 +48,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.3.vendor
 
 # Figerprint
 PRODUCT_PACKAGES += \
@@ -58,6 +59,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1.vendor
 
 # Graphics
 PRODUCT_PACKAGES += \
@@ -178,6 +183,12 @@ PRODUCT_PACKAGES += \
 # Renderscript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
+
+# RIL
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor
 
 # SamsungDoze
 PRODUCT_PACKAGES += \

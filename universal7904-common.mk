524be685d6c75db8f1a3b8469aee9bab239b318b
Tim Zimmermann <tim@linux4.de>
2021-10-31 17:25:02 +0100

    universal7904: Switch to AIDL libperfmgr power HAL
    Change-Id: I56928037bf543accb3175ea50c674f80a3f60b99
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -167,7 +167,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.universal7904
+    android.hardware.power-service.samsung-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
@@ -218,7 +221,10 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/samsung/aidl/power-libperfmgr
 
 # Thermal
 PRODUCT_PACKAGES += \

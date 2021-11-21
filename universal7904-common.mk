acb529c36d531fbc10542713d145548c31e30f5f
Tim Zimmermann <tim@linux4.de>
2021-11-21 08:57:14 +0100

    universal7904: Switch to new AIDL light HAL
    Change-Id: I7917b1da162e28187216fc8f55f164fae2ec8eef
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung
+    android.hardware.light-service.samsung
 
 # Media
 PRODUCT_COPY_FILES += \

c1444da3d726120e567a812baa7a476c2b437dfd
Tim Zimmermann <tim@linux4.de>
2021-11-23 21:40:36 +0100

    universal7904: Switch to new AIDL vibrator HAL
    Change-Id: If10972cce06dd5128729d4d7409c57f79775b09b
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -248,7 +248,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.3-service.samsung
+    android.hardware.vibrator-service.samsung
 
 # Wifi
 PRODUCT_PACKAGES += \

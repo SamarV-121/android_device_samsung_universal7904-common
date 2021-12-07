1c781afdb7211040af26ec0a975e8c45e120b078
SamarV-121 <samarvispute121@pm.me>
2021-12-07 11:50:34 +0530

    universal7904: Import some init scripts
    Change-Id: I4961a0f409038dd944415f35ccf1d8ea47f0ef3a
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -98,6 +98,8 @@ PRODUCT_PACKAGES += \
     fstab.enableswap \
     init.exynos7904.rc \
     init.exynos7904.usb.rc \
+    init.gps.rc \
+    init.vendor.rilcommon.rc \
     ueventd.exynos7904.rc \
     wifi_sec.rc
 

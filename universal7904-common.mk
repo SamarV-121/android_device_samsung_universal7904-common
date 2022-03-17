de7f86fc1856b31c34a17c6894c0cf9a098625af
SamarV-121 <samarvispute121@pm.me>
2022-03-17 22:09:58 +0530

    universal7904: Compile SamsungDAP
    Change-Id: Ia36d9f8f92fe842c38d1a31a5facd4f862bcacf0
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# DAP
+PRODUCT_PACKAGES += \
+    SamsungDAP
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \

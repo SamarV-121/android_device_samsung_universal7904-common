fa348fea0f072eb574c05716e2e27b95af37c334
Aaron Kling <webgeek1234@gmail.com>
2022-08-18 23:17:54 -0500

    universal7904: Remove references to a2dp module
    APM would fail to parse the config anyway:
    
      E DevicesFactoryHAL: loadAudioInterface couldn't load audio hw module audio.a2dp (No such file or directory)
      W DevicesFactoryHalHidl: The specified device name is not recognized: "a2dp"
      E AudioFlinger: loadHwModule() error -22 loading module a2dp
      W APM_AudioPolicyManager: could not open HW module a2dp
    
    Change-Id: Iaa1be881cfe8f8474cef0ba46e1b0a62b59e14be

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -16,7 +16,6 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \

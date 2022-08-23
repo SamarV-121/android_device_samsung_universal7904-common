a56aa2d28b3cae884064ea1fd9355f7242a458ef
SamarV-121 <samarvispute121@pm.me>
2022-08-23 13:43:13 +0530

    [DNM] universal7904: camera/provider: Add support for ultrawide camera
    Change-Id: I68a2f3ed00bbdeea0045ea680d24b8e0dda5348b

--- hidl/camera/provider/SamsungCameraProvider.cpp
+++ hidl/camera/provider/SamsungCameraProvider.cpp
@@ -26,6 +26,8 @@ using ::android::OK;
 const int kMaxCameraIdLen = 16;
 
 SamsungCameraProvider::SamsungCameraProvider() : LegacyCameraProviderImpl_2_5() {
+    mExtraIDs.push_back(52);
+
     if (!mInitFailed) {
         for (int i : mExtraIDs) {
             struct camera_info info;

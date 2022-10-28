86928e005fd8167a5722a4408371346426292534
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    universal7904: Use AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -286,7 +286,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     WifiOverlay \
     wpa_supplicant

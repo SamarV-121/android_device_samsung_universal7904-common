f6841bb696c7a24cb137091e4a5a249387170104
LuK1337 <priv.luk@gmail.com>
2022-11-03 20:57:11 +0100

    universal7904: Add hardware/samsung to soong namespaces
    See I78b1b9930a57bc01afda722b9f8a196c372f9d8b.
    
    Change-Id: I634b562e12b70a0c060d449d3ba1d4e87204cf5d

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -238,6 +238,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
 
 # Thermal

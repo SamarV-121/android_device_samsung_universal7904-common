8edb970b42281769c33c27e7f2378cac69c45fd6
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    universal7904: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -126,9 +126,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.samsung \
     com.android.nfc_extras \
-    libnfc-nci \
-    libnfc_nci_jni \
-    NfcNci \
     Tag
 
 PRODUCT_COPY_FILES += \

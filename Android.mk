87e124b299fa0f4bb075c64ef598123ff0de7086
Cole Faust <colefaust@google.com>
2023-10-13 12:32:19 -0700

    universal7904: Use install_symlink to add symlinks
    Using LOCAL_POST_INSTALL_CMD doesn't let the build system know that
    the symlinks exist, and breaks hermetic partition builds.
    
    Bug: 205632228
    Test: m installclean && m with aosp/2773149
    Change-Id: I3eddef663043a5de4eb60fb8bc82d0513aed55cb

--- Android.mk
+++ Android.mk
@@ -7,21 +7,4 @@ ifneq ($(filter m20lte m30lte a40 a30, $(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 
-EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 32 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(EGL_LIBS))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EGL 64 lib link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
 endif

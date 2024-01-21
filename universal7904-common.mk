40a64ad8f1093764ccd2e7a7c05bbd5958919e25
Tim Zimmermann <tim@linux4.de>
2024-01-21 20:08:37 +0100

    universal7904: Switch to AIDL fingerprint HAL
    Change-Id: Ic5bb3ed213bbdfe147c863aa7258bde4731e06dd

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -68,7 +68,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung
+    android.hardware.biometrics.fingerprint-service.samsung
 
 # FM Radio
 PRODUCT_PACKAGES += \

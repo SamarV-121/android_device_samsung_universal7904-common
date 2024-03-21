3eb4fc69e22117ac77bbb40bc8c23814cdf224af
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    universal7904: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -291,10 +291,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung

9c0e2b81313252b1a3ad48969467c20d2bf0c82d
SamarV-121 <samar@samarv121.dev>
2024-04-08 11:59:05 +0530

    universal7904: Hacky workaround to satisfy vintf kernel version check
    Change-Id: I306a55eabd6aab2cd7688c69b4fa0ac440cc6021

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,6 +59,8 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-l
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 ifeq ($(filter teegris, $(BOARD_SEPOLICY_TEE_FLAVOR)),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_mobicore.xml

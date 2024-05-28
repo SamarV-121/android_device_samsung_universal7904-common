8f1d5de5ecb88910a047581e7ace2d020081b697
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    universal7904: Allow to access bootable/deprecated-ota targets
    universal7904 is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to universal7904-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -294,6 +294,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung

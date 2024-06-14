6d427877db6f1e043f2ac8eab7afd70fb4687235
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    universal7904: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,7 +60,8 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-l
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_compatibility_matrix.xml
+    $(COMMON_PATH)/device_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 ifeq ($(filter teegris, $(BOARD_SEPOLICY_TEE_FLAVOR)),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_mobicore.xml

c0abe825c92bb6628b0a3360c821a73b06813fb7
Jan Altensen <info@stricted.net>
2025-02-07 21:07:11 +0100

    universal7904: Move to new fm vendor jni variable
    Change-Id: I48a4c0005a0761ca4c6ff574fca0153d2dbf54c9

--- universal7904-common.mk
+++ universal7904-common.mk
@@ -77,6 +77,8 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+$(call soong_config_set,libfmjni,vendor,slsi)
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \

c1ba729f723d998d1bfde22e5410eca9e6b2b47e
SamarV-121 <samar@samarv121.dev>
2025-03-02 21:46:32 +0530

    universal7904: Include slsi libbt namespace
    https://github.com/LineageOS/android_hardware_samsung_slsi_libbt/commit/5623d73d2c14f46e77e43fda14e0c037ad37bbf8
    
    Change-Id: I9d81ace04a717094ffc9b1e1eaf10ef1b4485d99

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,9 +25,6 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
-# Bluetooth
-BOARD_HAVE_BLUETOOTH_SLSI := true
-
 # Camera
 ifeq ($(TARGET_DEVICE),a40)
 SOONG_CONFIG_NAMESPACES += samsungCameraVars
--- universal7904-common.mk
+++ universal7904-common.mk
@@ -274,7 +274,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/samsung \
-    hardware/samsung_slsi-linaro/exynos/gralloc/gralloc3
+    hardware/samsung_slsi-linaro/exynos/gralloc/gralloc3 \
+    hardware/samsung_slsi/libbt
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile

1e29b246356f4b151fd8e26916fb2d189f256279
SamarV-121 <samar@samarv121.dev>
2025-06-11 12:31:25 +0530

    universal7904: Drop unused 64 bit audio blobs
    Change-Id: Iaece2d0622bc47d10123012ed3639dcc5bcac18f

--- extract-files.sh
+++ extract-files.sh
@@ -42,7 +42,7 @@ function blob_fixup {
 		xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aae30314aa/600e40f9820c805224008052e10315aa030080d2/g" | xxd -r -p > "${2}".patched
 		mv "${2}".patched "${2}"
 		;;
-        vendor/lib/libaudio_soundtrigger.so | vendor/lib/libaudioroute.universal7904.so | vendor/lib/soundfx/libaudioeffectoffload.so | vendor/lib64/soundfx/libaudioeffectoffload.so)
+        vendor/lib/libaudio_soundtrigger.so | vendor/lib/libaudioroute.universal7904.so | vendor/lib/soundfx/libaudioeffectoffload.so)
 		"$PATCHELF" --replace-needed libtinyalsa.so libtinyalsa.universal7904.so "$2"
 		;;
 	vendor/lib/hw/audio.primary.exynos7904.so)
